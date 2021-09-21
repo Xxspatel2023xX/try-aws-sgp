1b30c46e5c9244b8314f137c7fee03ae4835f751
Xxspatel2023xX <sarahpatel3@icloud.com>
2021-09-21 14:17:40 -0400

    20%

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Amplify from 'aws-amplify';
 import DisplayPosts from './components/DisplayPosts'
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
